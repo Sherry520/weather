1ca326a319cc990ae28ba10d7674232d18fcfc2b
BruceGuoYH <22561797+Sherry520@users.noreply.github.com>
2020-07-12 17:05:52 +0800

    4-12

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -6,12 +6,21 @@ const weatherMap = {
   'heavyrain': '大雨',
   'snow': '雪'
 }
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+ }
 Page({
   data: {
-    nowTemp: 12,
-    nowWeather: "多云"
+    nowTemp: "",
+    nowWeather: "sunny",
+    nowWeatherBackground: ""
   },
-  onLoad(){
+  getNow(callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
       data: {
@@ -25,9 +34,24 @@ Page({
         console.log(temp,weather)
         this.setData({
           nowTemp: temp + "°",
-          nowWeather: weatherMap[weather]
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: "/images/" + weather + "-bg.png"
         })
+        wx.setNavigationBarColor({
+          frontColor: '#ffffff',
+          backgroundColor: weatherColorMap[weather]
+        })
+      },
+      complete(){
+        callback && callback()
       }
     })
-  }
+  },
+  onLoad(){
+    this.getNow()
+  },
+  onPullDownRefresh(){
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+  })}
 })
