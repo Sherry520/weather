147b0da3f15001699c553f2de1d8dfcd47cd2ea1
BruceGuoYH <22561797+Sherry520@users.noreply.github.com>
2020-07-12 15:45:55 +0800

    4-10

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,3 +1,33 @@
+const weatherMap = {
+  'sunny': "晴天",
+  'cloundy': "多云",
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
 Page({
-  
+  data: {
+    nowTemp: 12,
+    nowWeather: "多云"
+  },
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp,weather)
+        this.setData({
+          nowTemp: temp + "°",
+          nowWeather: weatherMap[weather]
+        })
+      }
+    })
+  }
 })
